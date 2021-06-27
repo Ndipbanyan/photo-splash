d9de65f131c10ef7b69138b1c960554153e93d51
Ndipbanyan <agnesjedidiah09@gmail.com>
2021-06-27 20:39:28 +0100

    search

---
+++ photo-search/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { createApi } from "unsplash-js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <div className="search-area">
+          <div className="search-container">
+            <span class="fa fa-search" aria-hidden="true"></span>
+            <input
+              type="text"
+              className="search"
+              data-testid="search"
+              placeholder="Search for photo"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
